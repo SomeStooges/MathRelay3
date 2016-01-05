60d7b092e89cfd7675e09ac214474b53790d4587
Kyle Murdoch <kyle.murdoch576@gmail.com>
2016-01-05 17:46:23 -0600

    Fixes syntax error in control panel.

--- control_panel.php
+++ control_panel.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<script src="src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script type="text/javascript" src="./scripts/scripts_control_panel.js"></script>
 	</head>
 	<body>

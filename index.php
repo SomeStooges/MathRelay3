01bc89a1bf29efaa174af532272ccffa5bfaa3a0
ippinator <ippinator@gmail.com>
2016-01-05 12:28:41 -0600

    adding Roland's name in to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype HTML>
+<html>
+	<head>
+		<title> Welcome Page </title>
+	</head>
+	
+	<body>
+		<h1> WELCOME TO THE WORLD </h1>
+		<p> My name is Ethn Freeburg </p>
+		<p> My name is Roland Ip </p>
+	</body>
+</html>

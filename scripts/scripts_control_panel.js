25fd47569df8903806b277f0d9f56b6d1ad0e0d1
Kyle Murdoch <kyle.murdoch576@gmail.com>
2016-01-05 17:55:10 -0600

    Fixes more bugs.

--- scripts/scripts_control_panel.js
+++ scripts/scripts_control_panel.js
@@ -1,8 +1,8 @@
 // Script for the admin control panel
 $(document).ready( function() {
 	$("#reset_button").click( function() {
-		string action = 'adminReset';
-		$.post("/MathRelay3/server/admin_controls.php", action, function(data) {
+		var action = 'adminReset';
+		$.post("./server/admin_control.php", action, function(data) {
 			console.log(data);
 		});
 	});

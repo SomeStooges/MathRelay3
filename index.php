518f7404f1b20c261b16138d2035a25aaedffdf7
Kyle Murdoch <kyle.murdoch576@gmail.com>
2016-01-05 15:19:05 -0600

    Modifys scripts to allow both Windows and Linux to locate file path.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script type="text/javascript" src="./scripts/scripts_index.js"></script>
+		<script type="text/javascript" src=".\scripts\scripts_index.js"></script>
 		<title> Welcome Page </title>
 	</head>
 
@@ -12,6 +13,6 @@
 		<p> My name is Roland Ip </p>
 		<p> My name is Kyle Murdoch </p>
 
-		<button type="welcome_button"> Go to LOGIN </button>
+		<button id="welcome_button"> Go to LOGIN </button>
 	</body>
 </html>

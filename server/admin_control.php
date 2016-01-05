1f7ad8bbddb3f2bb330777d360f65900af815899
ippinator <ippinator@gmail.com>
2016-01-05 17:25:43 -0600

    set up function for adminReset

--- server/admin_control.php
+++ server/admin_control.php
@@ -16,12 +16,15 @@
 	} 
 	
 	//FUNCTION DEFINITIONS
+	function adminReset() {
+		return "Santosh was here! (adminReset() called)";
+	}
 	
 	//REQUEST SWITCH
 	$action = $_REQUEST['action'];
 	$return = true;
 	switch( $action ){
-		case 'getEvent': $return = getEvent(); break;
+		case 'adminReset': $return = adminReset(); break;
 	}
 	print json_encode($return);
 ?>

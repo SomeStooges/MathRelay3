c60fbb7ad428e647ea60ed908f0e6fd5ea6870ff
freeburgernator <freeburgernator@gmail.com>
2016-01-05 16:12:00 -0600

    Fixes script linkage bug

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 		<script type="text/javascript" src="./scripts/scripts_index.js"></script>
-		<script type="text/javascript" src="scripts/scripts_index.js"></script>
+		<script type="text/javascript" src="scripts\scripts_index.js"></script>
 		<title> Welcome Page </title>
 	</head>
 
--- scripts/scripts_index.js
+++ scripts/scripts_index.js
@@ -1,11 +1,10 @@
 // Java Script for the welcome page
 $(document).ready( function() {
-	$("welcome_button").click( function() {
+	$("#welcome_button").click( function() {
 		window.location.href = "user_login.php";
-		console.log("welcome_button was pushed");
 	});
 
-	$("admin_button").click( function() {
+	$("#admin_button").click( function() {
 		window.location.href = "admin_login.php";
 	});
 });

a7b1d136bfc72c9b5185a93c74696b6e83a729c6
Kyle Murdoch <kyle.murdoch576@gmail.com>
2016-01-05 22:04:45 -0600

    Fixes another syntax error.

--- user_login.php
+++ user_login.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<script type="text/javascript" src="/scripts_user_login.js"></script>
+		<script type="text/javascript" src="scripts/scripts_user_login.js"></script>
 		<title> User Login </title>
 	<head>
 

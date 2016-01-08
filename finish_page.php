0e3bbfc2fb6572f146bf8e2d1a8977fb350840d6
freeburgernator <freeburgernator@gmail.com>
2016-01-08 13:41:04 -0600

    Adds basic structure to finish and leaderboard.

---
+++ finish_page.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> Answer Sheet </title>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script type="text/javascript" src="scripts/scripts_finish_page.js"></script>
+	</head>
+
+	<body>
+		<h1> Finish Page </h1>
+		<!-- We can probably run the PHP from here and not a JQUERY post, except to reject the page if too premature -->
+		<p> <span id='rankMessage'><i>Some print out of rank or generic congradulation message </i></span></p>
+		<p> <span id='cleanupParagraph'><i>Some large paragraph of instructions for the team captain.</i></span></p>
+	</body>
+</html>
---
+++ leaderboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> Answer Sheet </title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script type="text/javascript" src="scripts/scripts_finish_page.js"></script>
+	</head>
+
+	<body>
+		<h1> Leaderboard </h1>
+		<p> <span id='leaderboardTable'> <i> Some table to display information in a fancy manner... </i> </span> </p>
+	</body>
+</html>

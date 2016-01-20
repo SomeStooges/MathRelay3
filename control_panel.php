5b39cbb88028ba2b2cdbeab7adc4dabb00fcbe00
freeburgernator <freeburgernator@gmail.com>
2016-01-19 20:42:21 -0600

    Adds the html framework for the activity log

--- control_panel.php
+++ control_panel.php
@@ -133,6 +133,17 @@
 				
 				<div id='tooltab4' style='background-color: #CCFFFF'>
 					<!-- Content for Team Activity log tab-->
+					<b> Team ID </b> <button id='freezeButton'>Freeze Log</button>
+					<table id='teamActivity'>
+						<?php
+							$numTeams = 50;
+							for($i=1;$i<=$numTeams;$i++){
+								print "<tr><td class='teamIDdiv'>";
+								print $i;
+								print "</td></tr>";
+							}
+						?>
+					<table>
 				</div>
 				
 				<div id='tooltab5' style='background-color: #FFFFCC'>
@@ -141,6 +152,7 @@
 				
 				<div id='tooltab6' style='background-color: #CCFFFF'>
 					<!-- Content for settings tab -->
+					
 				</div>
 			</div>
 		</div>	

116ffdb07fcebcbbcaf30de7f5a6d77cdaa101f1
ippinator <ippinator@gmail.com>
2016-02-07 23:46:33 -0600

    Adds incomplete graphical update that displays correct or incorrect answers

--- scripts/scripts_answer_sheet.js
+++ scripts/scripts_answer_sheet.js
@@ -18,7 +18,7 @@ function checkEvent(){
 }
 
 //Sends the answer to the server to be graded
-function gradeAnswer(qNum, l3, l2, l1){
+function gradeAnswer(qNum, l3, l2, l1, id1, id2, id3){
 	console.log('Sending answer: series: '+qNum+' ; '+l3+' ; '+l2+' ; '+l1+' ;');
 	obj = new Object();
 	obj.action = 'gradeAnswer';
@@ -26,6 +26,7 @@ function gradeAnswer(qNum, l3, l2, l1){
 	obj.level3 = l3;		//level 3 answer, as a char
 	obj.level2 = l2;		//level 2 answer, as a char
 	obj.level1 = l1;		//level 1 answer, as a char
+	//id1 = "'#" + id1 + "'";
 	$.post('server/user_runner.php',obj,function(data){
 		console.log(data);
 		data = JSON.parse(data);
@@ -33,10 +34,27 @@ function gradeAnswer(qNum, l3, l2, l1){
 		var res1 = data[1];	//result for level 1: 1 = correct, 0 = incorrect, 3 = too many attempts, 4 = already graded
 		var res2 = data[2];	//result for level 2
 		var res3 = data[3];	//result for level 3
+
 		console.log("hist "+hist+" ; "+res1+" "+res2+" "+res3);
-
 		//WRITE GUI CHANGE HERE
-
+		switch(res1){
+			case 0:	$('#'+id1).css('background-color','lightRed'); break;
+			case 1:	$('.level1Buttons').css('background-color','lightGreen'); break;
+			case 3:	$('.level1Buttons').css('background-color','black'); break;
+			case 4: $('.level1Buttons').css('background-color',''); break;
+		}
+		switch(res2){
+			case 0:	$('.level2Buttons').css('background-color','lightRed'); break;
+			case 1:	$('.level2Buttons').css('background-color','lightGreen'); break;
+			case 3:	$('.level2Buttons').css('background-color','black'); break;
+			case 4: $('.level2Buttons').css('background-color',''); break;
+		}
+		switch(res3){
+			case 0:	$('.level3Buttons').css('background-color','lightRed'); break;
+			case 1:	$('.level3Buttons').css('background-color','lightGreen'); break;
+			case 3:	$('.level3Buttons').css('background-color','black'); break;
+			case 4: $('.level3Buttons').css('background-color',''); break;
+		}
 		$('#currentPoints').html(data[4]);
 	});
 }
@@ -122,31 +140,31 @@ $(document).ready( function() {
 		//gets attempt history colors
 		//some GUI
 	});
-
+	var selid1, selid2, selid3;
 	$(".level3Buttons").click( function(){
-		selid = $(this).prop('id');
-		level3selected = selid.substring(3,4);
+		selid3 = $(this).prop('id');
+		level3selected = selid3.substring(3,4);
 		//SOME GUI CHANGE
 		$('.level3Buttons').css('background-color','');
 		$(this).css('background-color','rgb(5, 65, 198)');
 	});
 	$(".level2Buttons").click( function(){
-		selid = $(this).prop('id');
-		level2selected = selid.substring(3,4);
+		selid2 = $(this).prop('id');
+		level2selected = selid2.substring(3,4);
 		//SOME GUI CHANGE
 		$('.level2Buttons').css('background-color','');
 		$(this).css('background-color','rgb(5, 65, 198)');
 	});
 	$(".level1Buttons").click( function(){
-		selid = $(this).prop('id');
-		level1selected = selid.substring(3,4);
+		selid1 = $(this).prop('id');
+		level1selected = selid1.substring(3,4);
 		//SOME GUI CHANGE
 		$('.level1Buttons').css('background-color','');
 		$(this).css('background-color','rgb(5, 65, 198)');
 	});
 	$('#submit_answer').click( function(){
 		console.log("submitting answer");
-		gradeAnswer(seriesSelected,level3selected,level2selected,level1selected);
+		gradeAnswer(seriesSelected,level3selected,level2selected,level1selected,selid1,selid2,selid3);
 	});
 
 	$('#q1').click();

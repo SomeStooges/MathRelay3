8366e0f2f1e6d69e296e54f040b4a49e54159a8a
ippinator <ippinator@gmail.com>
2016-02-14 23:11:53 -0600

    minor changes to the timer

--- scripts/scripts_control_panel.js
+++ scripts/scripts_control_panel.js
@@ -4,6 +4,7 @@ var seconds = 0;
 var minutes = 0;
 var hours = 0;
 var t;
+var time;
 function timer() {
   t = setTimeout(add, 1000);
 }
@@ -18,7 +19,8 @@ function add() {
       hours++;
     }
   }
-  $("#timer").text((hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds));
+  time = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+  $("#timer").text(time);
   timer();
 }
 

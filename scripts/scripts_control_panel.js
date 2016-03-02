d2758d449559b935c0fd1e6e2a54c7db5b164919
ippinator <ippinator@gmail.com>
2016-03-02 00:21:28 -0600

    minor changes to graphics of control panel

--- scripts/scripts_control_panel.js
+++ scripts/scripts_control_panel.js
@@ -119,7 +119,7 @@ function updateEvent(uEvent){
   $.post('server/admin_control.php',obj, function(data){
     var bID = JSON.parse(data);
     $('.ribbonButton').css('background-color','');
-    $('#'+bID).css('background-color','#011858');
+    $('#'+bID).css('background-color','dimgray');
   });
 
   switch (ribbonID) {
@@ -187,7 +187,7 @@ function updateEvent(uEvent){
 function updateUI(){
   currentEvent = $("#cEvent").text().trim();
   console.log("Current Event: " + currentEvent);
-  $('#'+currentEvent).css('background-color','#011858');
+  $('#'+currentEvent).css('background-color','dimgray');
   console.log(currentEvent);
   toggleButtons(currentEvent);
 }

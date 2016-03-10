3e54b2ae6ced2ac4392cd1265d8007525fc1b355
ippinator <ippinator@gmail.com>
2016-03-09 19:09:56 -0600

    makes minor change to settings and control panel.

--- modules/m_settings.php
+++ modules/m_settings.php
@@ -32,7 +32,7 @@
 			<p id = 'passComplete'></p>
 		</div>
 		<div id='table3'>
-			<b> Reset Settings (Note: These changes will not be applied unless the RESET button is pressed!)</b>
+			<b> Regeneration Settings (Note: These changes will not be applied unless the REGENERATE button is pressed!)</b>
 			<table>
 				<tr><td>Number of Teams to Generate:</td><td><input id='numTeamsGen'></td><td><button id = 'saveTeams'>Save</button></td><td><span style = "color: green" id = 's1'></span></td></tr>
 				<tr><td>Number of Digits in Password to Generate:</td><td><input id='numDigPass'></td><td><button id = 'savePass'>Save</button></td><td><span style = "color: green" id = 's2'></span></td></tr>
@@ -42,7 +42,7 @@
 				<tr><td>Number of Questions</td><td><input id='numQuestions'></td></tr>
 			</table> -->
 			<br>
-			<p> <button id="reset_button">RESET</button> Push RESET to clear all points, change passwords, and change the number of teams. </p>
+			<p> <button id="reset_button">REGENERATE</button> Push REGENERATE to clear all points, change passwords, and change the number of teams. </p>
 		</div>
 	</body>
 </html>

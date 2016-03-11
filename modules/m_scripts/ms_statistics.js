9f0f1bb1752fa5b7c27a94bccd525ee65d626b1b
freeburgernator <freeburgernator@gmail.com>
2016-03-11 13:21:49 -0600

    Add chart.js and begins implementation

--- modules/m_scripts/ms_statistics.js
+++ modules/m_scripts/ms_statistics.js
@@ -1,40 +1,71 @@
 //Scripts for the m_statistics module
-function getStatistics() {
-  $.post('../server/admin_control.php', 'action=getStatistics', function(data) {
-    //TBD
-  });
+function range(start, finish) {
+    var r = [];
+    for (var i = start; i <= finish; i++) {
+        r.push(i);
+    }
+    return r;
 }
 
-$(document).ready( function(){
-  //getStatistics();
-var ctx = $("#myChart").get(0).getContext("2d");
+function getStatistics() {
+  $.post('../server/stat.php', "action=getStatistics", function(data) {
+    //console.log(data);
+    data = JSON.parse(data);
+    var attemptsByTime = data.attemptsByTime;
+    var correctByTime = data.correctByTime;
+    var scatterQuestionTime = data.scatterQuestionTime;
+    var attemptsByTeam = data.attemptsByTeam;
+    var correctByTeam = data.correctByTeam;
+    var attemptsByQuestion = data.attemptsByQuestion;
+    var correctByQuestion = data.correctByQuestion;
 
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July","August"],
+    //Add data to attemps/time line graph
+    var ctx = $("#attemptsVTime").get(0).getContext("2d");
+    var numMinutes = attemptsByTime.length;
+    console.log("DEBUG: attemptsByTime = " + attemptsByTime)
+    console.log("DEBUG: numMinutes = " + numMinutes);
+    var data = {
+    labels: range(1,numMinutes),
     datasets: [
         {
-            label: "My First dataset",
+            label: "Aggregate Attempts",
             fillColor: "rgba(220,220,220,0.2)",
             strokeColor: "rgba(220,220,220,1)",
             pointColor: "rgba(220,220,220,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40, 50]
+            data: attemptsByTime
         },
         {
-            label: "My Second dataset",
+            label: "Aggregate Correct Responses",
             fillColor: "rgba(151,187,205,0.2)",
             strokeColor: "rgba(151,187,205,1)",
             pointColor: "rgba(151,187,205,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90, 90]
+            data: correctByTime
         }
-    ]
-};
+      ]
+    };
+    var chart1 = new Chart(ctx).Line()
 
-var myLineChart = new Chart(ctx).Line(data);
+    //Add data to question/time scatter plot
+
+    //Add data to attempts/team bar graph
+
+    //add data to attempts/question bar graph
+
+  });
+}
+
+$(document).ready( function(){
+  //Bind line graph
+  //Bind scatter plot
+  //Bind bar graph 1
+  //Bind bar graph 2
+
+  getStatistics();
 
 });
--- modules/m_statistics.php
+++ modules/m_statistics.php
@@ -6,7 +6,8 @@
 <html>
   <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
+		<script src="../scripts/Chart.min.js"></script>
+		<script src="../scripts/Chart.Scatter.min.js"></script>
     <script type="text/javascript" src="m_scripts/ms_statistics.js"></script>
     <link rel="stylesheet" type="text/css" href="m_styles/mst_statistics.css">
 
@@ -14,6 +15,9 @@
   <body style='background-color: black'>
     <?php if(!function_exists('db_Query')){require $_SERVER['DOCUMENT_ROOT'] . 'MathRelay3/server/utilities.php';} ?>
     <button id='forceStatUpdate'>Update Now</button>
-		<canvas id="myChart" width="400" height="400"></canvas>
+		<canvas id="attemptsVTime" width="400" height="400"></canvas>
+		<canvas id="questionVTime" width="400" height="400"></canvas>
+		<canvas id="attemptsVTeam" width="400" height="400"></canvas>
+		<canvas id="attemptsVQuestion" width="400" height="400"></canvas>
   </body>
 </html>

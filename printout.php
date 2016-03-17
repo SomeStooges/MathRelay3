5a47a757da0a85cdca4d6890b3ec109f6bb0e579
ippinator <ippinator@gmail.com>
2016-03-17 17:15:38 -0500

    expands printing range

--- printout.php
+++ printout.php
@@ -17,44 +17,125 @@
 	</head>
 
 	<body>
-    <?php
-      if(!function_exists('db_Query')){
-        require $_SERVER['DOCUMENT_ROOT'] . 'MathRelay3/server/utilities.php';
-      }
+    <div id="page1">
+      <?php
+        if(!function_exists('db_Query')){
+          require $_SERVER['DOCUMENT_ROOT'] . 'MathRelay3/server/utilities.php';
+        }
 
-      $resource = db_Query("SELECT `team_id`,`password` FROM team_data;");
-      $teamData = array();
-      while($teamRow = mysqli_fetch_row($resource)){
-        $teamData[] = $teamRow;
-      }
+        $resource = db_Query("SELECT `team_id`,`password` FROM team_data;");
+        $teamData = array();
+        while($teamRow = mysqli_fetch_row($resource)){
+          $teamData[] = $teamRow;
+        }
 
-      print "<table id='teamDataTable1'>";
-      for($i=0;$i<29;$i++){
-        print "<tr class='page-break' id='dataRow" . $i . "'>";
-        for($j=0;$j<count($teamData[$i]);$j++){
-          if($j%2 ==1){
-            print "<td>Password: <b>" . $teamData[$i][$j] . "</b></td>";
+        print "<table id='teamDataTable1'>";
+        for($i=0;$i<29;$i++){
+          print "<tr class='page-break' id='dataRow" . $i . "'>";
+          for($j=0;$j<count($teamData[$i]);$j++){
+            if($j%2 ==1){
+              print "<td>Password: <b>" . $teamData[$i][$j] . "</b></td>";
+            }
+            if($j%2 ==0){
+              print "<td>Team ID: <b>" . $teamData[$i][$j] . "</b></td>";
+            }
           }
-          if($j%2 ==0){
-            print "<td>Team ID: <b>" . $teamData[$i][$j] . "</b></td>";
+          print "</tr>";
+        }
+        print "</table>";
+        if(count($teamData)> 29 && count($teamData) <= 58){
+          print "<table id='teamDataTable2'>";
+          for($i=29;$i<count($teamData);$i++){
+            print "<tr class='page-break' id='dataRow" . $i . "'>";
+            for($j=0;$j<count($teamData[$i]);$j++){
+              if($j%2 ==1){
+                print "<td>Password: <b>" . $teamData[$i][$j] . "</b></td>";
+              }
+              if($j%2 ==0){
+                print "<td>Team ID: <b>" . $teamData[$i][$j] . "</b></td>";
+              }
+            }
+            print "</tr>";
+          }
+        }else if(count($teamData)> 29 && count($teamData) > 58){
+          print "<table id='teamDataTable2'>";
+          for($i=29;$i<58;$i++){
+            print "<tr class='page-break' id='dataRow" . $i . "'>";
+            for($j=0;$j<count($teamData[$i]);$j++){
+              if($j%2 ==1){
+                print "<td>Password: <b>" . $teamData[$i][$j] . "</b></td>";
+              }
+              if($j%2 ==0){
+                print "<td>Team ID: <b>" . $teamData[$i][$j] . "</b></td>";
+              }
+            }
+            print "</tr>";
           }
         }
-        print "</tr>";
-      }
-      print "</table>";
-      print "<table id='teamDataTable2'>";
-      for($i=29;$i<count($teamData);$i++){
-        print "<tr class='page-break' id='dataRow" . $i . "'>";
-        for($j=0;$j<count($teamData[$i]);$j++){
-          if($j%2 ==1){
-            print "<td>Password: <b>" . $teamData[$i][$j] . "</b></td>";
+      ?>
+    </div>
+    <div id="page2">
+      <?php
+      if(count($teamData)>58 && count($teamData) <= 87){
+        print "<table id='teamDataTable3'>";
+        for($i=58;$i<count($teamData);$i++){
+          print "<tr class='page-break' id='dataRow" . $i . "'>";
+          for($j=0;$j<count($teamData[$i]);$j++){
+            if($j%2 ==1){
+              print "<td>Password: <b>" . $teamData[$i][$j] . "</b></td>";
+            }
+            if($j%2 ==0){
+              print "<td>Team ID: <b>" . $teamData[$i][$j] . "</b></td>";
+            }
           }
-          if($j%2 ==0){
-            print "<td>Team ID: <b>" . $teamData[$i][$j] . "</b></td>";
-          }
+          print "</tr>";
         }
-        print "</tr>";
+      }else if(count($teamData)>58 && count($teamData) >87){
+        print "<table id='teamDataTable3'>";
+        for($i=58;$i<87;$i++){
+          print "<tr class='page-break' id='dataRow" . $i . "'>";
+          for($j=0;$j<count($teamData[$i]);$j++){
+            if($j%2 ==1){
+              print "<td>Password: <b>" . $teamData[$i][$j] . "</b></td>";
+            }
+            if($j%2 ==0){
+              print "<td>Team ID: <b>" . $teamData[$i][$j] . "</b></td>";
+            }
+          }
+          print "</tr>";
+        }
       }
+      if(count($teamData)>87 && count($teamData) <= 116){
+        print "<table id='teamDataTable4'>";
+        for($i=87;$i<count($teamData);$i++){
+          print "<tr class='page-break' id='dataRow" . $i . "'>";
+          for($j=0;$j<count($teamData[$i]);$j++){
+            if($j%2 ==1){
+              print "<td>Password: <b>" . $teamData[$i][$j] . "</b></td>";
+            }
+            if($j%2 ==0){
+              print "<td>Team ID: <b>" . $teamData[$i][$j] . "</b></td>";
+            }
+          }
+          print "</tr>";
+        }
+      }else if(count($teamData)>87 && count($teamData) > 116){
+        print "<table id='teamDataTable4'>";
+        for($i=87;$i<116;$i++){
+          print "<tr class='page-break' id='dataRow" . $i . "'>";
+          for($j=0;$j<count($teamData[$i]);$j++){
+            if($j%2 ==1){
+              print "<td>Password: <b>" . $teamData[$i][$j] . "</b></td>";
+            }
+            if($j%2 ==0){
+              print "<td>Team ID: <b>" . $teamData[$i][$j] . "</b></td>";
+            }
+          }
+          print "</tr>";
+        }
+      }
+
     ?>
+  </div>
 	</body>
 </html>

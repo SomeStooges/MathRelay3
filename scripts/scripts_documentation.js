51714a4fc52a5aaa6ad5d5de79609fdfbbe22ea3
ippinator <ippinator@gmail.com>
2016-03-28 22:43:41 -0500

    fixes documentation page

--- scripts/scripts_documentation.js
+++ scripts/scripts_documentation.js
@@ -3,18 +3,16 @@ $(document).ready(function(){
   $('.selectorButton').click(function(){
     var target = $(this).attr('id');
     $('.selectorButton').css('background-color', '');
-    /*switch(target){
+    switch(target){
       case 'developerButton':
         $('#developers').show();
         $('#about').hide();
-        console.log('DEBUG: Developer Button Clicked!');
         break;
       case 'aboutButton':
         $('#about').show();
         $('#developers').hide();
-        console.log('DEBUG: About Button Clicked!');
         break;
-    }*/
+    }
     $('#'+target).css('background-color', 'rgb(32, 70, 93)');
   });
   $('#developerButton').click();

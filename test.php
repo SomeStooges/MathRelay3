a1942dd653aa4a0d9520c1468301c73ae11b5e68
ippinator <ippinator@gmail.com>
2016-03-30 14:29:52 -0500

    testing cleanup paragraph on test.php

--- test.php
+++ test.php
@@ -1,6 +1,17 @@
-<?php
-  session_start();
-  echo $_SERVER['SCRIPT_FILENAME'];
-  phpinfo()
-  //$_SESSION['admin']='Santosh';
-  ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+
+</head>
+<body>
+  <div id = "cleanUpParagraph">
+		<?php
+			$fileName = "server/cleanupParagraph.txt";
+			$myfile = fopen($fileName,'r');
+			$text = fread($myfile, filesize($fileName));
+			fclose($myfile);
+			print "<div id='cleanup'>".$text."</div>";
+		?>
+	</div>
+</body>
+</html>
